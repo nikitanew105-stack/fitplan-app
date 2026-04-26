dc5d4d7ce36a1b3729c05c19e7f8e4f45e7cb06d
nikitanew105-stack <nikitanew105@gmail.com>
2026-04-26 15:22:32 +0300

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -47,7 +47,7 @@ export default async function handler(req, res) {
     });
   }
 
-  res.status(200).json({
-    text: data.choices[0].message.content,
-  });
+ res.status(200).json({
+  raw: data,
+});
 }
